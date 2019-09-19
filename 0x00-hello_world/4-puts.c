29f6240947f26c9f3a96f144977ad1c6e74b2113
cedouiri <cedouiri@gmail.com>
2019-09-19 09:37:30 +0000

    fix error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: always 0 (success)
- */
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+* main - entry point
+* Return: always 0 (success)
+*/
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
