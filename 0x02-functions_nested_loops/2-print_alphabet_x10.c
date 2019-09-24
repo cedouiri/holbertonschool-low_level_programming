e9fa5b85bf86d1a52f658eb9022678bec67bf521
cedouiri <cedouiri@gmail.com>
2019-09-24 12:18:05 +0000

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+*print_alphabet_x10- use only user defined variables to create output
+*
+*Return: print to stdout the alphabet, in lowercase, followed by a new line.
+**/
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
