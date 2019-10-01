ab1229b0dc869bb5c84a177f648ae1b88009649e
cedouiri <cedouiri@gmail.com>
2019-10-01 22:53:33 +0000

    fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,38 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
-* _atoi - convert a string to an integer
-* @s: string.
-* Return: integer.
-**/
+ * _atoi - converests a string to an integer
+ * @s: string to convert
+ * Return: numbre
+ */
+
 
 int _atoi(char *s)
 {
+	int n, i, m;
 
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	short sh = 0;
-	int m = 0;
-
-	i = -1;
-	while (s[j] != '\0')
+	m = 1;
+	i = n = 0;
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if (s[j] == '-')
-			i = i * -1;
-
-		if (s[j] >= '0' && s[j] <= '9')
-		{
-			k = k * 10;
-			k = k - (s[j] - '0');
-			m = 1;
-		}
-		else if (m == 1)
-			break;
-		j++;
-		m++;
+		if (s[i] == '-')
+			m = m * -1;
+		i++;
 	}
-	k = k * i;
-	return (k);
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	m = m * -1;
+	return (n * m);
 }
