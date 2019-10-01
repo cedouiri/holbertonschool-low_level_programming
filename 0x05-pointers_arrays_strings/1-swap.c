d08ae66ea4ac7fafc9eef4dd75b2948eba019585
cedouiri <cedouiri@gmail.com>
2019-10-01 20:20:17 +0000

    add README

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* swap_int - swaps the values of two integers
+* @a: take an int
+* @b: take an int 
+**/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
