11aaf7ba9d73bb7831a0c30487eda6ad12951fec
cedouiri <cedouiri@gmail.com>
2019-10-23 22:34:17 +0000

    add 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - the main
+* @argc: arguments
+* @argv: size of argc
+* Return: always 0
+**/
+int main(int argc, char **argv)
+{
+	int x;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", x);
+	return (0);
+}
