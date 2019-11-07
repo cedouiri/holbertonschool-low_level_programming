5d2a9bece2a583399c52684a2515ae1ffb58edca
cedouiri <cedouiri@gmail.com>
2019-11-07 23:32:40 +0000

    fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,32 +2,29 @@
 #include "holberton.h"
 
 /**
-* binary_to_uint - a function that converts a binary
-*number to an unsigned int
+* binary_to_uint - a function that converts a
+*binary number to an unsigned int
 * @b: pointing to a string of 0 and 1 chars
-* Return: the converted number, or 0
+* Return: the converted number or 0
 **/
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int i;
-int len, base_2;
+	unsigned int i, int_n;
 
-if (!b)
-return (0);
-i = 0;
-for (len = 0; b[len] != '\0'; len++)
-;
-for (len--, base_2 = 1; len >= 0; len--, base_2 *= 2)
-{
-if (b[len] != '0' && b[len] != '1')
-{
-return (0);
-}
-if (b[len] & 1)
-{
-i += base_2;
-}
-}
-return (i);
+	int_n = 0;
+	i = 0;
+	if (!b)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		int_n <<= 1;
+		if (b[i] & 1)
+			int_n += 1;
+		i += 1;
+	}
+	return (int_n);
 }
