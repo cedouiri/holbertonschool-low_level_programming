2dbd8f8299b9a6c57b358ee2dc1e7967adff5b0a
cedouiri <cedouiri@gmail.com>
2019-11-07 22:56:01 +0000

    fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, int_n;
+unsigned int i;
+int len, base_2;
 
-	int_n = 0;
-	i = 0;
-	if (!b)
-		return (0);
-
-	while (b[i] != '\0')
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		int_n <<= 1;
-		if (b[i] & 1)
-			int_n += 1;
-		i += 1;
-	}
-	return (int_n);
+if (!b)
+return (0);
+i = 0;
+for (len = 0; b[len] != '\0'; len++)
+;
+for (len--, base_2 = 1; len >= 0; len--, base_2 *= 2)
+{
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
+}
+if (b[len] & 1)
+{
+i += base_2;
+}
+}
+return (i);
 }
