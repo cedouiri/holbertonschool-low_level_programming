c39b42ce0965dae0b2b962c08346aee47cdbe706
cedouiri <cedouiri@gmail.com>
2020-05-28 23:13:27 +0000

    declare new function

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,4 +3,6 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void prar(int *array, size_t start, size_t end);
 #endif
